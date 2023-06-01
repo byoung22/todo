7b8b1446c1f094ee39f07cce9cad3556997b82a0
Bailey <baileywyoung@gmail.com>
2023-06-01 13:45:30 -0700

    Modular ui design

--- src/project-list.js
+++ src/project-list.js
@@ -1,12 +1,7 @@
 import Project from './projects';
 
-let school = new Project('school');
-school.addTask('Homework', 'math hw', '5-5-12');
-school.addTask('Project', 'english essay', '6-6-12');
-
 export default class List {
     constructor () {
-        this.noDate = {};
         this.projects = {};
     }
 
--- src/render.js
+++ src/render.js
@@ -1,53 +1,142 @@
 import List from "./project-list";
-const navProj = document.querySelector('#projects');
-const overlay = document.querySelector('#overlay');
+import getDay from 'date-fns/getDay';
+import getMonth from 'date-fns/getMonth';
+import getDate from 'date-fns/getDate';
+import getYear from 'date-fns/getYear';
 
-// FORM
-const projectForm = document.querySelector('#project-form');
-const projectTitle = document.querySelector('#project-title'); 
-
-const taskForm = document.querySelector('#task-form');
-
-const projectFormButton = document.querySelector('#add-project');
-
+// Initialize
 const list = new List();
+list.addProject('None'); // For tasks with no category
+
+let currentPage = 'ToDo';
+// WORK ON LOADING TASKS SAVE ALL TASKS TO ALLTASK
+// SORT THE TASKS BY DATE
+
+const taskUI = {
+    alltasks: [],
+    display1: [],
+    display2: [],
+
+    taskDOM: function() {
+        this.tasksList = document.querySelector('#tasks');
+    },
+    taskRender: function() {
+        this.taskDOM();
+    }
+}
+
+const dateUI = {
+    date: new Date(),
+    dateDOM: function() {
+        this.dateDisplay = document.querySelector('#date-string');
+    },
+    dateString: function(date) {
+        let day = getDay(date);
+        let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        let month = getMonth(date);
+        let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        const dates = getDate(date);
+        const year = getYear(date);
+        let n = days[day] + ' ' + months[month] + ' ' + dates + ', ' + year;
+        this.dateDisplay.textContent = n;
+    },
+    dateRender: function() {
+        this.dateDOM();
+        this.dateString(this.date);
+    },
+}
+
+const navUI = {
+    navDOM: function() {
+        this.navProj = document.querySelector('#projects');
+    },
+    navProject: function() {
+        this.navProj.textContent = '';
+        Object.keys(list.projects).forEach(project => {
+            if (project === 'None') return;
+            let button = document.createElement('button');
+            button.textContent = project;
+            this.navProj.appendChild(button);
+        });
+    },
+    navRender: function() {
+        this.navDOM();
+        this.navProject();
+    },
+}
+
+const formUI = {
+    formDOM: function() {
+        this.projectForm = document.querySelector('#project-form');
+        this.projectFormButton = document.querySelector('#add-project');
+        this.projectTitle = document.querySelector('#project-title');
+        this.overlay = document.querySelector('#overlay');
+
+        this.taskForm = document.querySelector('#task-form');
+        this.taskFormButton = document.querySelector('#add-task');
+        this.taskSelection = document.querySelector('#category');
+        this.taskTitle = document.querySelector('#title');
+        this.taskDescription = document.querySelector('#Description');
+        this.taskDate = document.querySelector('#date');
+    },
+    projectSelect: function() {
+        this.taskSelection.innerHTML = '';
+        let options = '<option value="None">Choose a Project</option>';
+        Object.keys(list.projects).forEach(project => {
+            if (project === 'None') return;
+            options += `<option value="${project}">${project}</option>`;
+        });
+
+        this.taskSelection.innerHTML = options;
+    },
+    formButton: function() {
+        this.overlay.addEventListener('click', function() {
+            formUI.taskForm.classList.add('hidden');
+            formUI.projectForm.classList.add('hidden');
+            formUI.overlay.classList.add('hidden');
+        });
+        
+        this.projectForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            list.addProject(formUI.projectTitle.value);
+            updateProject();
+            formUI.projectForm.classList.add('hidden');
+            formUI.overlay.classList.add('hidden');
+        });
+    
+        this.projectFormButton.addEventListener('click', () => {
+            formUI.projectForm.classList.remove('hidden');
+            formUI.overlay.classList.remove('hidden');
+        });
+    
+        this.taskForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            formUI.projectForm.classList.add('hidden');
+            formUI.overlay.classList.add('hidden');
+        });
+    
+        this.taskFormButton.addEventListener('click', () => {
+            formUI.taskForm.classList.remove('hidden');
+            formUI.overlay.classList.remove('hidden');
+        });
+    },
+    formRender: function() {
+        this.formDOM();
+        this.formButton();
+        this.projectSelect();
+    },
+}
 
 // Initial data
 list.addProject('🏫 School');
 list.addProject('💼 Work');
 list.projects['🏫 School'].addTask('Homework', 'math hw', '5-5-12');
-console.log(list);
-
-
-function renderNav() {
-    navProj.textContent = '';
-    Object.keys(list.projects).forEach(project => {
-        let button = document.createElement('button');
-        button.textContent = project;
-        navProj.appendChild(button);
-    })
-    console.log();
-}
-function appendListeners() {
-    overlay.addEventListener('click', function() {
-        taskForm.classList.add('hidden');
-        projectForm.classList.add('hidden');
-        overlay.classList.add('hidden');
-    })
-    projectForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        list.addProject(projectTitle.value)
-        renderNav();
-        projectForm.classList.add('hidden');
-        overlay.classList.add('hidden');
-    });
-    projectFormButton.addEventListener('click', () => {
-        projectForm.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
-}
 
 export default function renderUI() {
-    renderNav();
-    appendListeners();
+    dateUI.dateRender();
+    formUI.formRender();
+    navUI.navRender();
+    taskUI.taskRender();
+
+    console.log(list);
 }
